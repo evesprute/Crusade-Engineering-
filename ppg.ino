c77a59dedbb099b03b9217a7e1675e0f8d4d1179
amyzhang90 <165714870+amyzhang90@users.noreply.github.com>
2026-04-29 14:29:58 -0700

    Add files via upload

---
+++ ppg.ino
@@ -0,0 +1,55 @@
+#include <Wire.h>
+#include "MAX30105.h"
+#include "heartRate.h"
+
+MAX30105 particleSensor;
+
+const byte RATE_SIZE = 4;
+byte rates[RATE_SIZE];
+byte rateSpot = 0;
+long lastBeat = 0;
+
+float beatsPerMinute;
+int beatAvg;
+
+void setup() {
+  Serial.begin(115200);
+
+  if (!particleSensor.begin(Wire, I2C_SPEED_STANDARD)) {
+    Serial.println("Sensor not found");
+    while (1);
+  }
+
+  particleSensor.setup();
+  particleSensor.setPulseAmplitudeRed(0x0A);
+  particleSensor.setPulseAmplitudeIR(0x0A);
+}
+
+void loop() {
+  long irValue = particleSensor.getIR();
+
+  if (checkForBeat(irValue)) {
+    long delta = millis() - lastBeat;
+    lastBeat = millis();
+
+    beatsPerMinute = 60 / (delta / 1000.0);
+
+    if (beatsPerMinute < 255 && beatsPerMinute > 20) {
+      rates[rateSpot++] = (byte)beatsPerMinute;
+      rateSpot %= RATE_SIZE;
+
+      beatAvg = 0;
+      for (byte x = 0; x < RATE_SIZE; x++)
+        beatAvg += rates[x];
+      beatAvg /= RATE_SIZE;
+    }
+
+    Serial.print("BPM: ");
+    Serial.print(beatsPerMinute);
+    Serial.print(" Avg BPM: ");
+    Serial.println(beatAvg);
+  }
+
+  Serial.print("IR=");
+  Serial.println(irValue);
+}
